7f0d438ce742c8e35a6ff2b57823e927369aca54
Kalv1 <calvin.lambert@protonmail.com>
2021-11-03 14:04:24 +0100

    :sparkles: Add controller / view

---
+++ src/lehangar/control/HangarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace lehangar\controller;
+
+use mf\control\AbstractController;
+
+class HangarController extends AbstractController
+{
+    public function construct() {
+        parent::__construct();
+    }
+}
---
+++ src/lehangar/view/HangarView.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lehangar\view;
+
+use mf\view\AbstractView;
+
+class HangarView extends AbstractView
+{
+
+    protected function renderBody($selector)
+    {
+        /**
+         * Switch pour le choix du render.
+         */
+    }
+}
