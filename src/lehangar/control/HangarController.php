cc997c84208b759a371fbb864af4cd9cdb811e6b
Eisenpic <eisenpic@hotmail.fr>
2021-11-03 16:48:57 +0100

    :sparkles: Making ProduitView

--- src/lehangar/control/HangarController.php
+++ src/lehangar/control/HangarController.php
@@ -2,11 +2,22 @@
 
 namespace lehangar\controller;
 
+use lehangar\model\Produit;
+use lehangar\model\Producteur;
+use lehangar\model\Categorie;
+use lehangar\view\HangarView;
 use mf\control\AbstractController;
+use mf\router\Router;
 
 class HangarController extends AbstractController
 {
     public function construct() {
         parent::__construct();
+    }
+
+    public function viewProduit(){
+        $produits = Produit::select()->get();
+        $view = new HangarView($produits);
+        $view->render('produit');
     }
 }
--- src/lehangar/view/HangarView.php
+++ src/lehangar/view/HangarView.php
@@ -6,11 +6,66 @@ use mf\view\AbstractView;
 
 class HangarView extends AbstractView
 {
+    public function __construct($data)
+    {
+        parent::__construct($data);
+    }
 
     protected function renderBody($selector)
     {
         /**
          * Switch pour le choix du render.
          */
+        $html = $this->renderHeader();
+
+        switch ($selector) {
+            case 'produit';
+                $html .= $this->renderProduit();
+                break;
+        }
+
+        $html .= $this->renderFooter();
+
+        return $html;
+    }
+
+    private function renderProduit()
+    {
+        $html = "
+            <section>
+                <div>
+                    <div> <!-- div avec overflow: scroll -->
+                        ";
+
+        foreach ($this->data as $produit) {
+            $html .= "
+                            <div>
+                                <div>
+                                    <p>$produit->nom</p>
+                                    <p>$produit->prix €</p>
+                                    <p>$produit->producteur->nom</p>
+                                    <form action='../AjouterPanier/' method='post'>
+                                    <input type='hidden' name='produit' value='$produit'>
+                                    <select name='quantite'>
+                                        <option value=''>--Please choose an option--</option>";
+
+            for ($i = 0; $i < 21; $i++){
+                $html .= "<option value='$i'>$i</option>";
+            }
+            $html .= "</select>
+                                        <input type='submit' value='Ajouter au panier'>
+                                    </form>
+                                </div>
+                            </div>
+                        ";
+        }
+
+        $html .= "
+                    </div>
+                </div>
+            </section>
+        ";
+
+        return $html;
     }
 }
